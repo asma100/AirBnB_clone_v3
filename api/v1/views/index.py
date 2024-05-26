35f824250acf421487b46c1f326f035584abc3b8
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-05-26 11:49:20 -0700

    3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Index file for the API
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """ Returns the status of the API """
